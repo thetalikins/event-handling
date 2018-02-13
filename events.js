43c37eec0883bde180dcb9b6f1d9db9fbbc28e2f
Theta Likins <thetalikins@Thetas-MacBook-Air.local>
2018-02-14 12:01:52 +1300

    mkGreen for div2

--- events.js
+++ events.js
@@ -7,6 +7,7 @@ function start () {
   // The first example is done for you. This will change the background colour of the first div
   // when you mouse over it.
   one()
+  two()
     
   // Your turn! Create a new function called `two`, then call it from here.
 }
@@ -23,7 +24,12 @@ function one () {
 }
 
 // CREATE FUNCTION two HERE
+function two () {
+  var two = document.getElementById('two')
 
+  two.addEventListener('mouseenter', makeGreen)
+  two.addEventListener('mouseleave', makeWhite)
+}
 // CREATE FUNCTION three HERE
 
 // CREATE FUNCTION four HERE
